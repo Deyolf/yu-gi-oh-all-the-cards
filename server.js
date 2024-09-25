ea4799760bbb32e6f6a8238536078948d5930f5d
Deyolf <colombofabio06@gmail.com>
2024-09-25 11:57:35 +0200

    Update server.js
    rimozione della cache sulla homepage per permettere la gestione degli hotswap

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ var express = require('express');
 var app = express();
 var fs = require("fs");
 var DB_json
-var Homepage
 
 var bodyParser = require('body-parser')
 app.use(bodyParser.json());
@@ -14,6 +13,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function (req, res) {
     res.setHeader('Content-Type', 'text/html; charset=utf-8');
+    fs.readFile(`${__dirname}/html/home.html`, 'utf8', function (err, data) {
+        Homepage = data
+    })
     res.write(Homepage);
     res.end();
 });
@@ -36,7 +38,4 @@ app.post('/', function (req, res) {
 })
 var server = app.listen(port, hostname, () => {
     console.log(`Express App running at http://${hostname}:${port}/`);
-    fs.readFile(`${__dirname}/html/home.html`, 'utf8', function (err, data) {
-        Homepage = data
-    })
 })
