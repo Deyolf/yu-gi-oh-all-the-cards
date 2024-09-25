987b429dffce7792b209e93f426a9fdb4df1ca21
Deyolf <colombofabio06@gmail.com>
2024-09-25 10:41:14 +0200

    dio can manenti

---
+++ server.js
@@ -0,0 +1,43 @@
+const hostname = '127.0.0.1';
+const port = 12000;
+
+
+var express = require('express');
+var app = express();
+var fs = require("fs");
+var DB_json
+var Homepage
+
+var bodyParser = require('body-parser')
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', function (req, res) {
+   res.setHeader('Content-Type', 'text/html; charset=utf-8');
+   let response = []
+   DB_json.forEach(famiglia => {
+      id_famiglia = famiglia.id_famiglia
+      id_compo = famiglia.id_compo
+      response.push({ id_famiglia, id_compo })
+   });
+   res.end(JSON.stringify(response));
+});
+app.get('/:id', function (req, res) {
+   res.setHeader('Content-Type', 'application/json; charset=utf-8');
+   var users = DB_json;
+   var user = users[req.params.id]
+   res.end(JSON.stringify(user));
+})
+app.post('/', function (req, res) {
+   res.setHeader('Content-Type', 'application/json; charset=utf-8');
+   var users = DB_json;
+   var user = req.body;
+   users[users.length] = user
+   res.end(JSON.stringify(users));
+})
+var server = app.listen(port, hostname, () => {
+   console.log(`Express App running at http://${hostname}:${port}/`);
+   fs.readFile(`${__dirname}/html/home.html`, 'utf8', function (err, data) {
+      Homepage = JSON.parse(data)
+   })
+})
