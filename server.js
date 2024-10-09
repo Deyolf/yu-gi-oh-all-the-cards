704267acad76ead4d1e4b39e8d2d69ad3ba81fdc
Deyolf <colombofabio06@gmail.com>
2024-10-09 10:49:33 +0200

    fix pagina yugioh

--- server.js
+++ server.js
@@ -5,9 +5,10 @@ const port = 12000;
 var express = require('express');
 var app = express();
 var fs = require("fs");
+const path = require("path");
+app.use(express.static(path.join(__dirname, "static")));
 var DB_json
 
-var Homepage
 var yugioh
 var cardfightvanguard
 var lordoftherings
@@ -21,31 +22,71 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function (req, res) {
     res.setHeader('Content-Type', 'text/html; charset=utf-8');
-    res.end(Homepage);
+    res.status(200)
+
+    const filePath = path.join(__dirname, "static", "html/home.html");
+    res.write(fs.readFileSync(filePath, "utf-8"));
+
+    res.end();
 });
 
 app.get('/:html', function (req, res) {
     res.setHeader('Content-Type', 'text/html');
     let risorsa
-    if(req.params.html == "yugioh"){
-        res.write(yugioh);
+    if (req.params.html == "yugioh") {
+        res.setHeader('Content-Type', 'text/html; charset=utf-8');
+        res.status(200)
+
+        const filePath = path.join(__dirname, "static", "html/yugioh.html");
+        res.write(fs.readFileSync(filePath, "utf-8"));
+
+        res.end();
     }
-    else if(req.params.html == "cardfightvanguard"){
-        res.write(cardfightvanguard);
+    else if (req.params.html == "cardfightvanguard") {
+        res.setHeader('Content-Type', 'text/html; charset=utf-8');
+        res.status(200)
+
+        const filePath = path.join(__dirname, "static", "html/cardfightvanguard.html");
+        res.write(fs.readFileSync(filePath, "utf-8"));
+
+        res.end();
     }
-    else if(req.params.html == "lordoftherings"){
-        res.write(lordoftherings);
+    else if (req.params.html == "lordoftherings") {
+        res.setHeader('Content-Type', 'text/html; charset=utf-8');
+        res.status(200)
+
+        const filePath = path.join(__dirname, "static", "html/lordoftherings.html");
+        res.write(fs.readFileSync(filePath, "utf-8"));
+
+        res.end();
     }
-    else if(req.params.html == "magicthegathering"){
-        res.write(magicthegathering);
+    else if (req.params.html == "magicthegathering") {
+        res.setHeader('Content-Type', 'text/html; charset=utf-8');
+        res.status(200)
+
+        const filePath = path.join(__dirname, "static", "html/magicthegathering.html");
+        res.write(fs.readFileSync(filePath, "utf-8"));
+
+        res.end();
     }
-    else if(req.params.html == "pokemon"){
-        res.write(pokemon);
+    else if (req.params.html == "pokemon") {
+        res.setHeader('Content-Type', 'text/html; charset=utf-8');
+        res.status(200)
+
+        const filePath = path.join(__dirname, "static", "html/pokemon.html");
+        res.write(fs.readFileSync(filePath, "utf-8"));
+
+        res.end();
     }
-    else if(req.params.html == "dragonball"){
-        res.write(dragonball);
+    else if (req.params.html == "dragonball") {
+        res.setHeader('Content-Type', 'text/html; charset=utf-8');
+        res.status(200)
+
+        const filePath = path.join(__dirname, "static", "html/dragonball.html");
+        res.write(fs.readFileSync(filePath, "utf-8"));
+
+        res.end();
     }
-    res.end();
 })
 app.post('/', function (req, res) {
     res.setHeader('Content-Type', 'application/json; charset=utf-8');
@@ -56,25 +97,4 @@ app.post('/', function (req, res) {
 })
 var server = app.listen(port, hostname, () => {
     console.log(`Express App running at http://${hostname}:${port}/`);
-    fs.readFile(`${__dirname}/html/home.html`, 'utf8', function (err, data) {
-        Homepage = data
-    })
-    fs.readFile(`${__dirname}/html/yugioh.html`, 'utf8', function (err, data) {
-        yugioh = data
-    })
-    fs.readFile(`${__dirname}/html/dragonball.html`, 'utf8', function (err, data) {
-        dragonball = data
-    })
-    fs.readFile(`${__dirname}/html/lordoftherings.html`, 'utf8', function (err, data) {
-        lordoftherings = data
-    })
-    fs.readFile(`${__dirname}/html/pokemon.html`, 'utf8', function (err, data) {
-        pokemon = data
-    })
-    fs.readFile(`${__dirname}/html/cardfightvanguard.html`, 'utf8', function (err, data) {
-        cardfightvanguard = data
-    })
-    fs.readFile(`${__dirname}/html/magicthegathering.html`, 'utf8', function (err, data) {
-        magicthegathering = data
-    })
 })
--- static/script/yugioh.js
+++ static/script/yugioh.js
@@ -24,14 +24,13 @@ let cards = [];
       cardsToDisplay.forEach(card => {
         const cardElement = document.createElement('div');
         cardElement.classList.add('col-lg-2', 'col-md-4', 'col-sm-6', 'mb-4');
-        cardElement.innerHTML = `
-          <div class="card bg-secondary text-light" style="width: 100%;">
+        cardElement.innerHTML =
+          `<div class="card" style="width: 100%;">
             <img src="${card.card_images[0].image_url}" class="card-img-top" alt="${card.name}" onclick="mostraDettagliCarta(${JSON.stringify(card).replace(/"/g, '&quot;')})" data-bs-toggle="modal" data-bs-target="#cardModal">
             <div class="card-body">
               <h5 class="card-title">${card.name}</h5>
             </div>
-          </div>
-        `;
+          </div>`;
         cardsContainer.appendChild(cardElement);
       });
     }
@@ -63,7 +62,7 @@ let cards = [];
 
     function getDisplayedPages(currentPage, totalPages) {
       const displayedPages = [];
-      const range = 1; // Numero di pagine da visualizzare
+      const range = 2; // Numero di pagine da visualizzare
 
       for (let i = Math.max(1, currentPage - range); i <= Math.min(totalPages, currentPage + range); i++) {
         displayedPages.push(i);
@@ -140,11 +139,19 @@ let cards = [];
     }
 
     function mostraDettagliCarta(card) {
+      // Mostra il modal con i dettagli della carta
       document.getElementById('modalCardName').innerText = card.name;
       document.getElementById('modalCardDescription').innerText = card.desc;
       document.getElementById('modalCardType').innerText = card.type;
       document.getElementById('modalCardAttack').innerText = card.atk || 'N/A';
       document.getElementById('modalCardDefense').innerText = card.def || 'N/A';
+
+      // Mostra l'immagine nel modal
+      document.getElementById('modalImage').src = card.card_images[0].image_url;
+
+      // Mostra il modal
+      const cardModal = new bootstrap.Modal(document.getElementById('cardModal'));
+      cardModal.show();
     }
 
     function showSuggestions() {
